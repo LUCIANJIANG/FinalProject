db8b4531ceac91046443f97948bd4607f1efeba0
BT_1E10_16 <sample@bths.edu>
2022-06-10 12:34:54 -0400

    update

--- src/Current.java
+++ src/Current.java
@@ -5,15 +5,6 @@ public class Current {
     private String example;
     private String synonyms;
 
-    public Current(String word, String definition,String partOfSpeech, String example, String synonyms)
-    {
-        this.word = word;
-        this.definition = definition;
-        this.partOfSpeech = partOfSpeech;
-        this.example = example;
-        this.synonyms = synonyms;
-    }
-
     public String getWord()
     {
         return word;
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         WordGUI gui = new WordGUI();
 
-
+// TESTING CODE
 //        try
 //        {
 //            HttpRequest request = HttpRequest.newBuilder()
